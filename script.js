e5c13625995cbb453657f170f09b8f7f82bc3000
kai10107 <kaiaustin@icloud.com>
2024-03-05 15:34:37 +1100

    fetchs shuffle api and removes some logs

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-const deckUrl = "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
-let deckId = '';
+const deckUrl =
+  "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+let deckId = "";
 
 fetch(deckUrl)
-    .then(response => response.json()) 
-    .then(data => {
-        deckId = data.deck_id;
-        console.log(data); 
-    })
-    .catch(error => console.error("Error fetching deck:", error));
-
-    const drawButton = document.getElementById('draw-card');
-    const cardDisplay = document.getElementById('card-display');
-
-    drawButton.addEventListener('click', () => {
-      fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-          .then(response => response.json())
-          .then(data => {
-            const card = data.cards[0]; 
-            const cardImage = document.createElement('img');
-            cardImage.src = card.image;
-            cardDisplay.appendChild(cardImage); 
-              
-          })
-          .catch(error => console.error("Error drawing a card:", error));
+  .then((response) => response.json())
+  .then((data) => {
+    deckId = data.deck_id;
+    console.log(data);
   });
 
-   const shuffleButton = document.getElementById('shuffle-deck'); 
+const drawButton = document.getElementById("draw-card");
+const cardDisplay = document.getElementById("card-display");
+const shuffleButton = document.getElementById("shuffle-deck");
 
-   shuffleButton.addEventListener('click', () => {
-    
-  
+drawButton.addEventListener("click", () => {
+  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+    .then((response) => response.json())
+    .then((data) => {
+      const card = data.cards[0];
+      const cardImage = document.createElement("img");
+      cardImage.src = card.image;
+      cardDisplay.appendChild(cardImage);
+    });
+});
+
+shuffleButton.addEventListener("click", () => {
+  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`);
+  console.log("Deck shuffled!");
 });
