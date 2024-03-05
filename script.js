97fef9068569c17fccefbbe613f06062a2a48b3b
kai10107 <kaiaustin@icloud.com>
2024-03-05 16:25:34 +1100

    adds input button can now choose how many cards to draw

--- script.js
+++ script.js
@@ -6,25 +6,31 @@ fetch(deckUrl)
   .then((response) => response.json())
   .then((data) => {
     deckId = data.deck_id;
-    console.log(data);
-  });
+    const drawUrl = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=`;
 
-const drawButton = document.getElementById("draw-card");
-const cardDisplay = document.getElementById("card-display");
-const shuffleButton = document.getElementById("shuffle-deck");
+    const drawButton = document.getElementById("draw-card");
+    const cardDisplay = document.getElementById("card-display");
+    const shuffleButton = document.getElementById("shuffle-deck");
 
-drawButton.addEventListener("click", () => {
-  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-    .then((response) => response.json())
-    .then((data) => {
-      const card = data.cards[0];
-      const cardImage = document.createElement("img");
-      cardImage.src = card.image;
-      cardDisplay.appendChild(cardImage);
+    drawButton.addEventListener("click", () => {
+      const cardCount = document.getElementById("card-count").value;
+      const updatedDrawUrl = drawUrl + cardCount;
+
+      fetch(updatedDrawUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          const cards = data.cards;
+          cardDisplay.innerHTML = "";
+
+          cards.forEach((card) => {
+            const cardImage = document.createElement("img");
+            cardImage.src = card.image;
+            cardDisplay.appendChild(cardImage);
+          });
+        });
     });
-});
 
-shuffleButton.addEventListener("click", () => {
-  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`);
-  console.log("Deck shuffled!");
-});
+    shuffleButton.addEventListener("click", () => {
+      fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`);
+    });
+  });
