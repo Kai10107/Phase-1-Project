d5628c8218f441d5379dd0069a912c0d9c3d144d
kai10107 <kaiaustin@icloud.com>
2024-03-06 13:46:35 +1100

    adds calculatescore function

--- script.js
+++ script.js
@@ -35,60 +35,75 @@ fetch(deckUrl)
     });
   });
 
+let playerHand = [];
+let dealerHand = [];
+let playerScore = 0;
+let gameStarted = false;
 
-  let playerHand = [];
-  let dealerHand = [];
-  let playerScore = 0;
-  let gameStarted = false;
+const dealButton = document.getElementById("deal-button");
 
-  const dealButton = document.getElementById("deal-button");
-
-   dealButton.addEventListener('click', () => {
-   if (gameStarted) {
+dealButton.addEventListener("click", () => {
+  if (gameStarted) {
     alert("Please finish the current game before starting a new one.");
-    return;  
-    }
-    fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`)
-   
-      
-    })
-    playerHand = [];
-    dealerHand = [];
-    playerScore = 0;
-    gameStarted = true; 
+    return;
+  }
+  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`);
+});
+playerHand = [];
+dealerHand = [];
+playerScore = 0;
+gameStarted = true;
 
-    fetch(drawUrl + '2') 
-    .then(response => response.json())
-    .then(data => {
-        playerHand = data.cards;
-        updatePlayerHandDisplay(); 
-      }); 
+fetch(drawUrl + "2")
+  .then((response) => response.json())
+  .then((data) => {
+    playerHand = data.cards;
+    updatePlayerHandDisplay();
+  });
 
-        fetch(drawUrl + '1')
-        .then(response => response.json())
-        .then(data => {
-            dealerHand.push(data.cards[0]); 
-             updateDealerHandDisplay();
-        });
-        
-
-    
+fetch(drawUrl + "1")
+  .then((response) => response.json())
+  .then((data) => {
+    dealerHand.push(data.cards[0]);
+    updateDealerHandDisplay();
+  });
 
 function updatePlayerHandDisplay() {
- const updatePlayerHandDisplay = document.getElementById('player-hand');
- PlayerHandDisplay.innerHTML = '';
- playerHand.forEach(card => {
-  let cardImage = document.createElement('img');
-  cardImage.src = card.image;
-  playerHandDisplay.appendChild(cardImage);
- });
+  const updatePlayerHandDisplay = document.getElementById("player-hand");
+  PlayerHandDisplay.innerHTML = "";
+  playerHand.forEach((card) => {
+    let cardImage = document.createElement("img");
+    cardImage.src = card.image;
+    playerHandDisplay.appendChild(cardImage);
+  });
 }
 
+function updateDealerHandDisplay() {
+  const dealerHandDisplay = document.getElementById("dealer-hand");
+  dealerHandDisplay.innerHTML = "";
+  let cardImage = document.createElement("img");
+  cardImage.src = 'Pictures/back.png'; 
+  dealerHandDisplay.appendChild(cardImage);
+}
 
-function updateDealerHandDispaly() {
- const dealerHandDisplay = document.getElementById('dealer-hand');
- dealerHandDisplay.innerHTML = '';
- let cardImage = document.createElement('img');
- cardImage.src = card.image;
- dealerHandDisplay.appendChild(cardImage);
+function calculatescore (){
+  let score = 0;
+  let hasAce = false;
+
+  for (const card of hand) {
+    let cardValue = (card.value);
+
+    if (Card.value === "JACK" || card.value === "QUEEN" || card.value === "KING") {
+      cardValue = 10;
+    }  else if (card.value === "ACE") {
+      hasAce = true;
+      cardValue = 11;
+ 
+    }
+    score += cardValue;
+  }
+  if (hasAce && score > 21) {
+    score -= 10; 
+}
+return score;
 }
