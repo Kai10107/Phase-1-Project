3407404c8c03bb19a2aec44fe0c817e75e999e92
kai10107 <kaiaustin@icloud.com>
2024-03-06 16:58:35 +1100

    adds playroundbutton function

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 const deckUrl = "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+const drawUrl = "https://www.deckofcardsapi.com/api/deck/";
 
 fetch(deckUrl)
   .then(response => response.json())
   .then(data => {
-    const deckId = data.deck_id;
+    deckId = data.deck_id;
    
-  });
-
-  const drawUrl = "https://www.deckofcardsapi.com/api/deck/";
-
-function drawCards(count) {
+  function drawCards(count) {
     const updatedDrawUrl = drawUrl + deckId + "/draw/?count=" + count;
 
-    fetch(updatedDrawUrl)
-        .then(response => response.json())
-        .then(data => {
+    fetch(updatedDrawUrlUrl)
+    .then(response => response.json())
+    .then(data => {
+      const drawnCards = data.cards;
             
-            const drawnCards = data.cards; 
-
-           
-        });
+    });    
 } 
+
+const playButton = document.getElementById("play-round");
+const roundResult = document.getElementById("round-result"); 
+
+playButton.addEventListener("click", () => {
+    drawCards(2); 
+
+    if (drawnCards[0].value > drawnCards[1].value) {
+        roundResult.textContent = "Player Wins the Round!";
+    } else if (drawnCards[0].value < drawnCards[1].value) {
+        roundResult.textContent = "Computer Wins the Round!";
+    } else {
+        roundResult.textContent = "It's a Tie!";
+    }
+  });
+});
